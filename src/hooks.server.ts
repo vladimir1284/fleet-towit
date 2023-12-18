080567fd3d912f8978ff95b65fc5cb2302929f16
vladimir1284 <vladimir.rdguez@gmail.com>
2023-12-18 15:37:39 -0500

    Initial commit

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { sequence } from "@sveltejs/kit/hooks";
+import { handleErrorWithSentry, sentryHandle } from "@sentry/sveltekit";
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+  dsn: 'https://264c6d3e8448a85d1a3717e5ef22a502@o4506418139299840.ingest.sentry.io/4506418143035392',
+  tracesSampleRate: 1.0,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { sentrySvelteKit } from "@sentry/sveltekit";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [sentrySvelteKit({
+        sourceMapsUploadOptions: {
+            org: "towit",
+            project: "javascript-sveltekit"
+        }
+    }), sveltekit()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+});
