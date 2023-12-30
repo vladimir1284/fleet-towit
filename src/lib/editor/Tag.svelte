ebf50ce004682a6b6858993e048f0cea8792a8a8
vladimir1284 <vladimir.rdguez@gmail.com>
2023-12-30 15:17:09 -0500

    Creating tags inside the editor

---
+++ src/lib/editor/Tag.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	// import { tipexEditor } from './editor_store';
+	import { tipexEditor } from '@friendofsvelte/tipex';
+
+	import { onMount } from 'svelte';
+	onMount(() => {
+		console.log($tipexEditor);
+	});
+
+	// TODO: open a modal for creating a tag, specifying the name and the type of the variable
+	function createTag() {
+		$tipexEditor?.commands.deleteSelection();
+		$tipexEditor?.commands.insertContent('{{ new_tag }}');
+	}
+</script>
+
+<button class="tipex-edit-button" on:click={createTag}>
+	<iconify-icon icon="majesticons:curly-braces"></iconify-icon>
+</button>
--- src/routes/editor/+page.svelte
+++ src/routes/editor/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { PageData } from './$types';
-
+	import Tag from '$lib/editor/Tag.svelte';
 	export let data: PageData;
 	import { Tipex } from '@friendofsvelte/tipex';
 	export let htmlContent = ``;
@@ -10,4 +10,8 @@
 	{htmlContent}
 	style="margin-top: 1rem; margin-bottom: 0;"
 	className="h-[70vh] border border-neutral-200 overflow-y-auto"
-/>
+>
+	<svelte:fragment slot="utilities">
+		<Tag />
+	</svelte:fragment>
+</Tipex>
