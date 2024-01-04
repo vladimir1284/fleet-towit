3a827313299fca19c450ccbade96dcac8d004ae9
vladimir1284 <vladimir.rdguez@gmail.com>
2024-01-03 22:20:46 -0500

    Do not use prerender

--- src/lib/editor/Tag.svelte
+++ src/lib/editor/Tag.svelte
@@ -2,11 +2,6 @@
 	// import { tipexEditor } from './editor_store';
 	import { tipexEditor } from '@friendofsvelte/tipex';
 
-	import { onMount } from 'svelte';
-	onMount(() => {
-		console.log($tipexEditor);
-	});
-
 	// TODO: open a modal for creating a tag, specifying the name and the type of the variable
 	function createTag() {
 		$tipexEditor?.commands.deleteSelection();
