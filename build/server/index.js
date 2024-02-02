11200f098de01fefdcb51e0879516319847c4d1b
vladimir1284 <vladimir.rdguez@gmail.com>
2024-02-01 22:27:00 -0500

    remove build

--- build/server/index.js
+++
@@ -1,9 +0,0 @@
-import './chunks/prod-ssr-7cc47430.js';
-import './chunks/paths-57a6d830.js';
-import './chunks/index-d41d8d8d.js';
-import './chunks/stringify-f45e10e0.js';
-import './chunks/index2-c76cf199.js';
-export { S as Server } from './chunks/index-cb9db167.js';
-import './chunks/_layout.server.ts-50fb6d34.js';
-import './chunks/ssr-f8fe68b4.js';
-//# sourceMappingURL=index.js.map
