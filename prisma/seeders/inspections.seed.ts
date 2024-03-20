d0d3714d0970f77e7805a62e9c287c36203955ec
raulodev <raulodev@gmail.com>
2024-03-20 17:13:23 -0400

    create seed

--- prisma/seeders/inspections.seed.ts
+++ prisma/seeders/inspections.seed.ts
@@ -1,169 +1,183 @@
 const seedInspection = async (prisma, tenantsId: number[]) => {
 	console.log('Seeding inspections data...');
 
+	const siNo = {
+		create: [
+			{
+				name: 'Si'
+			},
+			{
+				name: 'No'
+			}
+		]
+	};
+
+	const buenoRR = {
+		create: [
+			{
+				name: 'Bueno'
+			},
+			{
+				name: 'R.R'
+			}
+		]
+	};
+
 	for (const tenantId of tenantsId) {
 		await prisma.customForm.create({
 			data: {
 				name: 'Custom Form',
 				tenantId: tenantId,
-				fields: {
+				cards: {
 					create: [
+						// Whinch
 						{
-							name: 'Control de respuesto',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Si' }, { name: 'No' }]
+							name: 'Whinch',
+							fields: {
+								create: [
+									{
+										name: 'Control de repuesto',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									},
+									{
+										name: 'Control Regular',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									},
+									{
+										name: 'Cargar desde el camion',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									},
+									{
+										name: 'Condiciones del cable y el winch',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'No. de baterias grandes',
+										type: 'NUMBER'
+									},
+									{
+										name: 'Batería Volt',
+										type: 'NUMBER'
+									}
+								]
 							}
 						},
+						// Frente
 						{
-							name: 'Control regular',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Si' }, { name: 'No' }]
+							name: 'Frente',
+							fields: {
+								create: [
+									{
+										name: 'Adaptador',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Condiciones del Pasador',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Tornillo',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Soldadura',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Cadena',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Sticker',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Cable 7 vias',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Interruptor del winch',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									}
+								]
 							}
 						},
-
+						// PTO
 						{
-							name: 'Carga desde el camión',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Si' }, { name: 'No' }]
+							name: 'PTO',
+							fields: {
+								create: [
+									{
+										name: 'Sistema de gravedad',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									},
+									{
+										name: 'Funcionamiento',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Instalacion electrica',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Capacidad del liquido hidraulico',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Sujecion del PTO',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Caja del PTO Seguridad',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									},
+									{
+										name: 'Pistones sellos',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Mangueras de los pistones',
+										type: 'SINGLE_CHECK',
+										checkOptions: siNo
+									}
+								]
 							}
 						},
-
+						// Rampas de extension
 						{
-							name: 'Condiciones del cable y el winch',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Adaptador',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Condiciones del pasador',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Tornillo',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Soldadura',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Cadena',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-
-						{
-							name: 'Sticker',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Cable 7 vías',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'interruptor del winch',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Si' }, { name: 'No' }]
-							}
-						},
-
-						{
-							name: 'Sistema de gravedad',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Si' }, { name: 'No' }]
-							}
-						},
-						{
-							name: 'Funcionamiento',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Instalación eléctrica',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Capacidad del líquido hidraulico',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'Requiere rellenar' }]
-							}
-						},
-						{
-							name: 'Sujecion del PTO',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Caja del PTO Seguridad',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Si' }, { name: 'No' }]
-							}
-						},
-						{
-							name: 'Pistones sellos',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Manguera de los pistones',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Rampas de extensión hacia delante',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
-							}
-						},
-						{
-							name: 'Rampas de extensión hacia atras',
-							type: 'SINGLE_CHECK',
-							checkOptions: {
-								create: [{ name: 'Bueno' }, { name: 'R.R' }]
+							name: 'Rampas de extension',
+							fields: {
+								create: [
+									{
+										name: 'Hacia adelante',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									},
+									{
+										name: 'Hacia atras',
+										type: 'SINGLE_CHECK',
+										checkOptions: buenoRR
+									}
+								]
 							}
 						}
 					]
