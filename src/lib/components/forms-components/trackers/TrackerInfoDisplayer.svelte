79f8277d410cf850b429792903c6447a3db7acbc
LuisUlloaC <luis.ulloa75360@gmail.com>
2024-04-19 18:36:37 -0400

    Update: Updated tracker functionality

---
+++ src/lib/components/forms-components/trackers/TrackerInfoDisplayer.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import dayjs from 'dayjs';
+	import relativeTime from 'dayjs/plugin/relativeTime'
+	dayjs.extend(relativeTime)
+
+    export let data: any;
+</script>
+<div class="flex flex-col">
+    <strong>{data.vehicle.tracker.name}</strong>
+    <span>{data.vehicle.type + '-' + data.vehicle.make + '-' + data.vehicle.model}</span>
+    <span>Vin: {data.vehicle.vin}</span>
+    <span>Plate: {data.vehicle.plate.plate || '-'}</span>
+    <span>Lat: {data.latitude}</span>
+    <span>Lng: {data.longitude}</span>
+    <span>Time: {dayjs().to(data.timeStamp)}</span>
+</div>
